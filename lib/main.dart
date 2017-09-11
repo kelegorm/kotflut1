9d739eba67afff386c77dd84e627f7a9d253c46c
kelegorm <kelegorm1@gmail.com>
2017-09-11 18:34:14 +0300

    Added google sign in Added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,30 @@
+import 'dart:async';
+
 import 'package:flutter/widgets.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 import 'MainApp.dart';
 
+
+final googleSignIn = new GoogleSignIn();
+
+final fAuth = FirebaseAuth.instance;
+
+
+Future<Null> _ensureLoggedIn() async {
+  if (googleSignIn.currentUser == null)
+    await googleSignIn.signInSilently();
+  if (googleSignIn.currentUser == null)
+    await googleSignIn.signIn();
+
+  if (await fAuth.currentUser() == null) {
+    var creds = await googleSignIn.currentUser.authentication;
+    await fAuth.signInWithGoogle(idToken: creds.idToken, accessToken: creds.accessToken);
+  }
+}
+
 void main() {
   runApp(new MainApp());
+  _ensureLoggedIn();
 }
