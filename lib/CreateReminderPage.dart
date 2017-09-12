3e94ef35ee1131958176465a042ddb6d9072b22c
kelegorm <kelegorm1@gmail.com>
2017-09-12 19:15:01 +0300

    Made buttons in Create Reminder screen works

--- lib/CreateReminderPage.dart
+++ lib/CreateReminderPage.dart
@@ -5,20 +5,16 @@ import 'dart:async';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:kot_flut_1/main.dart';
-
-
-typedef OnSubmitted<String>(String value);
+import 'package:meta/meta.dart';
 
 
 class CreateReminderPage extends StatelessWidget {
 
-  final OnSubmitted onSubmitted;
-
   final DatabaseReference _reminders;
 
 
-  CreateReminderPage({this.onSubmitted, DatabaseReference ref}) : _reminders = ref
+  CreateReminderPage({@required DatabaseReference ref})
+      : _reminders = ref
   {
     assert(_reminders != null);
   }
@@ -26,50 +22,63 @@ class CreateReminderPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    String reminderText;
+
     return new Scaffold(
-      appBar: new AppBar(
-        title: new Text("Reminder"),
-        textTheme: Theme.of(context).textTheme,
-        iconTheme: Theme.of(context).iconTheme,
-        backgroundColor: Colors.white,
-        actions: <Widget>[
-          new IconButton(icon: new Icon(Icons.schedule), onPressed: () {}),
-          new MaterialButton(
-            onPressed: () => {},
-            child: new Text("Save".toUpperCase()),
-            textColor: Colors.blue,
-          )
-        ],
-        //leading: new Text("leading"), //todo set close icon
-      ),
-      backgroundColor: new Color(0xffe0e0e0),
-      body: new Column(
-        children: <Widget>[
-          new DecoratedBox(
-            decoration: new BoxDecoration(
-              color: new Color(0xffffffff),
-            ),
-            child: new ListTile(
-              title: new TextField(
-                decoration: new InputDecoration(
-                  isDense: true,
-                  hideDivider: true,
-                  helperText: 'helper text',
-                  hintText: 'Remember to...',
-                  hintStyle: Theme.of(context).textTheme.subhead.copyWith(color: Colors.black54.withAlpha(170)),
-                ),
-                style: Theme.of(context).textTheme.subhead.copyWith(color: Colors.black),
-                autofocus: true,
-                onSubmitted: (text) => _textField_submitted(text, context),
-              ),
-              leading: new Icon(
-                Icons.note,
-                color: Colors.blue,
-            ),
-            ),
+        backgroundColor: new Color(0xffe0e0e0),
+
+        appBar: new AppBar(
+          title: new Text("Reminder"),
+          textTheme: Theme.of(context).textTheme,
+          iconTheme: Theme.of(context).iconTheme,
+          backgroundColor: Colors.white,
+          // BACK BUTTON
+          leading: new IconButton(
+              icon: new Icon(Icons.close),
+              onPressed: () => _navigationBack(context)
           ),
-        ]
-      )
+          actions: <Widget>[
+            // SCHEDULE BUTTON
+            new IconButton(
+                icon: new Icon(Icons.schedule),
+                onPressed: () {}),// Todo
+            // SAVE BUTTON
+            new MaterialButton(
+              child: new Text("Save".toUpperCase()),
+              textColor: Colors.blue,
+              onPressed: () => _textField_submitted(reminderText, context),
+            )
+          ],
+        ),
+
+        body: new Column(
+          children: <Widget>[
+            new DecoratedBox(
+              decoration: new BoxDecoration(
+                color: new Color(0xffffffff),
+              ),
+              child: new ListTile(
+                title: new TextField(
+                  decoration: new InputDecoration(
+                    isDense: true,
+                    hideDivider: true,
+                    helperText: 'helper text',
+                    hintText: 'Remember to...',
+                    hintStyle: Theme.of(context).textTheme.subhead.copyWith(color: Colors.black54.withAlpha(170)),
+                  ),
+                  style: Theme.of(context).textTheme.subhead.copyWith(color: Colors.black),
+                  autofocus: true,
+                  onSubmitted: (text) => _textField_submitted(text, context),
+                  onChanged: (text) => reminderText = text,
+                ),
+                leading: new Icon(
+                  Icons.note,
+                  color: Colors.blue,
+              ),
+              ),
+            ),
+          ]
+        )
     );
   }
 
@@ -78,10 +87,10 @@ class CreateReminderPage extends StatelessWidget {
       'text':text,
       //'creatorName': googleSignIn.currentUser.displayName,
     });
+    _navigationBack(context);
+  }
 
-
-//    if (onSubmitted != null) onSubmitted(text);
-
+  void _navigationBack(context) {
     if (Navigator.canPop(context)) {
       Navigator.pop(context);
     } else {
